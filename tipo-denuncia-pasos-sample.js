ce533a597b7cbd2a656f14f036d32aefd7cb486b
el-sonny <sonny@spaceshiplabs.com>
2015-08-05 12:25:36 -0500

    arbitrary

--- tipo-denuncia-pasos-sample.js
+++ tipo-denuncia-pasos-sample.js
@@ -1,27 +1,39 @@
-module.exports = {
-	"tipos de denuncias" : [
-		{
-			"handle" : "bullying",
-			"nombre" : "Bullying",
-			"estados" : [
-				{
-					"paso" : "director",
-					"opciones" : [1,4]
-				},
-				{
-					"paso" : "telefono",
-					"opciones" : [1,4],
-					"variables" : {
-						"telefono" : "01 800 222 2676",
-						"horario" : "8 am a 8:30 pm"
-					}
-				},
-				{
-					"paso" : "canalizacion",
-					"opciones" :[3,]
-				}
+"tipos de denuncias" : [
+	{
+		"handle" : "bullying",
+		"titulo" : "Bullying",
+		"estados" : [
+			{
+				"paso" : "director",
+				"opciones" : [1,4],
+			},
 
-			]
-		}
-	]
-}
+			{
+				"paso" : "telefono",
+				"opciones" : [1,4],
+				"variables" : {
+					"telefono" : "01 800 222 2676",
+					"horario" : "8 am a 8:30 pm"
+				}
+			},
+			{
+				"paso" : "canalizacion",
+				"opciones" :[3,4]
+			},
+			{
+				"paso" : "canalizacion",
+
+			}
+
+		]
+	},
+	{
+		"handle" : "infraestructura",
+		"titulo" : "Infraestructura",
+		"estados" : [
+			{
+				"paso" : "director",
+			}
+		]
+	}
+]
